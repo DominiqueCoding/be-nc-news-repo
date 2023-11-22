a92b85dac5348e72cbdbe97f0c30288cc1935d2d
DominiqueCoding <aanon12589@gmail.com>
2023-11-22 14:46:34 +0000

    finish endpoints for /api/articles

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {handlePsqlErrors, handle500Errors, handleCustomErrors } = require('./error')
 const { getTopics } = require('./controller/topicsController')
 const { getApi } = require('./controller/apiController')
-const { getArticleById } = require('./controller/articlesController')
+const { getArticleById, getAllArticles } = require('./controller/articlesController')
 const app = express()
 
 app.get('/api/topics',getTopics)
@@ -10,6 +10,8 @@ app.get('/api/topics',getTopics)
 app.get('/api',getApi)
 
 app.get("/api/articles/:article_id",getArticleById)
+
+app.get("/api/articles",getAllArticles)
 
 app.use(handlePsqlErrors)
 app.use(handleCustomErrors)
--- controller/articlesController.js
+++ controller/articlesController.js
@@ -1,4 +1,5 @@
-const { selectArticleById } = require("../model/articlesModel")
+const articles = require("../db/data/test-data/articles")
+const { selectArticleById, selectAllArticles } = require("../model/articlesModel")
 const { checkIfIdExists } = require("../utils")
 
 
@@ -9,6 +10,13 @@ exports.getArticleById = (req,res,next) =>{
     selectArticleById(id)
     .then((article)=>{
         res.status(200).send(article)
+    })
+    .catch(next)
+}
+
+exports.getAllArticles = (req,res,next) =>{
+    selectAllArticles().then((articles)=>{
+        res.status(200).send(articles)
     })
     .catch(next)
 }
